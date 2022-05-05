7c75e0943f9b615fbb70c3c3e3c3d338c99ababb
lenwind <lenwind@163.com>
2022-05-05 22:57:15 +0800

    02.refactor test, add parameterized test.

--- container/src/test/java/pers/lenwind/container/ContainerTest.java
+++ container/src/test/java/pers/lenwind/container/ContainerTest.java
@@ -2,29 +2,35 @@ package pers.lenwind.container;
 
 import jakarta.inject.Inject;
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Named;
 import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
 import pers.lenwind.container.exception.BaseException;
 import pers.lenwind.container.exception.CyclicDependencyException;
 import pers.lenwind.container.exception.DependencyNotFoundException;
 
 import java.util.Optional;
 import java.util.Set;
+import java.util.stream.Stream;
 
 import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.params.provider.Arguments.arguments;
 
 public class ContainerTest {
+    private ContextConfiguration contextConfiguration;
+
+    @BeforeEach
+    void setUp() {
+        contextConfiguration = new ContextConfiguration();
+    }
+
     @Nested
-    class ComponentConstruction {
-        private ContextConfiguration contextConfiguration;
-
-        @BeforeEach
-        void setUp() {
-            contextConfiguration = new ContextConfiguration();
-        }
-
+    class TypeBind {
         @Test
-        void should_bind_instance_to_context() {
+        void should_bind_specific_instance_to_context() {
             Component instance = new Component() {
             };
             contextConfiguration.bind(Component.class, instance);
@@ -33,47 +39,109 @@ public class ContainerTest {
             assertSame(instance, component);
         }
 
-        @Test
-        void should_throw_exception_while_not_instantiable() {
-            contextConfiguration.bind(Component.class, AbstractComponent.class);
+        @ParameterizedTest(name = "bind {0}")
+        @MethodSource("typeSource")
+        void should_bind_type_to_context(Class<? extends Component> type) {
+            Dependency dependency = new Dependency() {
+            };
+            contextConfiguration.bind(Component.class, type);
+            contextConfiguration.bind(Dependency.class, dependency);
 
-
-            assertThrows(BaseException.class,
-                () -> new Context(contextConfiguration.getComponentProviders()).get(Component.class));
+            GetDependency component = (GetDependency) new Context(contextConfiguration.getComponentProviders()).get(Component.class).get();
+            assertEquals(dependency, component.geDependency());
         }
 
-        static abstract class AbstractComponent implements Component {
+
+        static Stream<Arguments> typeSource() {
+            return Stream.of(arguments(Named.of("Construction inject type", ConstructionInject.class)),
+                arguments(Named.of("Method inject type", MethodInject.class)),
+                arguments(Named.of("Field inject type", FieldInject.class)));
         }
 
+        static class ConstructionInject implements Component, GetDependency {
 
-        @Test
-        void should_return_empty_if_not_bind_to_context() {
-            Optional<Component> component = new Context(contextConfiguration.getComponentProviders()).get(Component.class);
+            Dependency dependency;
 
-            assertTrue(component.isEmpty());
+            @Inject
+            public ConstructionInject(Dependency dependency) {
+                this.dependency = dependency;
+            }
+            @Override
+            public Dependency geDependency() {
+                return dependency;
+            }
+
         }
+        static class MethodInject implements Component, GetDependency {
 
-        @Test
-        void should_throw_exception_if_dependency_not_found() {
-            contextConfiguration.bind(Component.class, InstanceWithInject.class);
+            Dependency dependency;
 
-            DependencyNotFoundException exception = assertThrows(DependencyNotFoundException.class, () -> new Context(contextConfiguration.getComponentProviders()));
-            assertEquals(InstanceWithInject.class, exception.getInstanceType());
-            assertEquals(Dependency.class, exception.getDependencyType());
+            @Inject
+            public void setDependency(Dependency dependency) {
+                this.dependency = dependency;
+            }
+            @Override
+            public Dependency geDependency() {
+                return dependency;
+            }
+
         }
+        static class FieldInject implements Component, GetDependency {
 
-        @Test
-        void should_throw_exception_if_cyclic_transitive_dependency() {
-            contextConfiguration.bind(Component.class, InstanceWithInject.class);
-            contextConfiguration.bind(Dependency.class, DependencyWithAnotherDependency.class);
-            contextConfiguration.bind(AnotherDependency.class, DependencyWithBean.class);
+            @Inject
+            Dependency dependency;
+            @Override
+            public Dependency geDependency() {
+                return dependency;
+            }
 
-            CyclicDependencyException exception = assertThrows(CyclicDependencyException.class, () -> new Context(contextConfiguration.getComponentProviders()));
-            Set<Class<?>> dependencies = Set.of(InstanceWithInject.class, DependencyWithAnotherDependency.class, DependencyWithBean.class);
-            assertTrue(exception.getDependencies().containsAll(dependencies));
         }
+        interface GetDependency {
+            Dependency geDependency();
 
+        }
     }
+
+
+    @Test
+    void should_throw_exception_while_not_instantiable() {
+        contextConfiguration.bind(Component.class, AbstractComponent.class);
+
+        assertThrows(BaseException.class,
+            () -> new Context(contextConfiguration.getComponentProviders()).get(Component.class));
+    }
+
+    static abstract class AbstractComponent implements Component {
+    }
+
+
+    @Test
+    void should_return_empty_if_not_bind_to_context() {
+        Optional<Component> component = new Context(contextConfiguration.getComponentProviders()).get(Component.class);
+
+        assertTrue(component.isEmpty());
+    }
+
+    @Test
+    void should_throw_exception_if_dependency_not_found() {
+        contextConfiguration.bind(Component.class, InstanceWithInject.class);
+
+        DependencyNotFoundException exception = assertThrows(DependencyNotFoundException.class, () -> new Context(contextConfiguration.getComponentProviders()));
+        assertEquals(InstanceWithInject.class, exception.getInstanceType());
+        assertEquals(Dependency.class, exception.getDependencyType());
+    }
+
+    @Test
+    void should_throw_exception_if_cyclic_transitive_dependency() {
+        contextConfiguration.bind(Component.class, InstanceWithInject.class);
+        contextConfiguration.bind(Dependency.class, DependencyWithAnotherDependency.class);
+        contextConfiguration.bind(AnotherDependency.class, DependencyWithBean.class);
+
+        CyclicDependencyException exception = assertThrows(CyclicDependencyException.class, () -> new Context(contextConfiguration.getComponentProviders()));
+        Set<Class<?>> dependencies = Set.of(InstanceWithInject.class, DependencyWithAnotherDependency.class, DependencyWithBean.class);
+        assertTrue(exception.getDependencies().containsAll(dependencies));
+    }
+
 
     @Nested
     class DependencySelection {
